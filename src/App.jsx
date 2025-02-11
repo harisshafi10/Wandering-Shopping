0125ac4aa3c9ffa926b096b11cc80813caf2762c
harisshafi10 <harisshafi69@gmail.com>
2025-02-11 23:01:56 +0530

    fixed the title

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import HomePage from "./components/pages/HomePage";
-import Navbar from "./components/Navbar"
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
